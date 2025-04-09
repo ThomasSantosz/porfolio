81b55eca72e7bccc2159294610a4ea94d7a4ae1e
Thomas Santos <thomas19123110@gmail.com>
2025-04-09 15:38:39 -0400

    Updates

---
+++ assets/HowWeDolt-Ck8F3nB9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D-u54hu3.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"How We Do It"}),e.jsx("p",{children:'This is the "How We Do It" page.'})]});export{s as default};
