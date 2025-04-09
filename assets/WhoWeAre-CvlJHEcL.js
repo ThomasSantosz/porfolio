64bdd5362e30b784edfc2f38b2102a8dad9ade5a
Thomas Santos <thomas19123110@gmail.com>
2025-04-09 08:25:37 -0400

    Updates

--- assets/WhoWeAre-CvlJHEcL.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C6eNOjV-.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Who We Are"}),e.jsx("p",{children:'This is the "Who We Are" page.'})]});export{s as default};
