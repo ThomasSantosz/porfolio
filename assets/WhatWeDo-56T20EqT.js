23f16aaf9fba0a3e232e864df01ac0cf44361b0b
Thomas Santos <thomas19123110@gmail.com>
2025-04-09 20:53:21 -0400

    Updates

---
+++ assets/WhatWeDo-56T20EqT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DbIjo5Go.js";const t=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"How We Do It"}),e.jsx("p",{children:'This is the "How We Do It" page.'})]});export{t as default};
