daf66b79e1ec3590384faa6853ac2e341c8d7528
Thomas Santos <thomas19123110@gmail.com>
2025-04-12 20:47:20 -0400

    Updates

---
+++ assets/WhatWeBelieve-C5eA0BPg.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D-jmBJ_y.js";const t=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"How We Do It"}),e.jsx("p",{children:'This is the "How We Do It" page.'})]});export{t as default};
--- assets/WhoWeAre-BNKJ9-8R.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChKxTUv4.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Who We Are"}),e.jsx("p",{children:'This is the "Who We Are" page.'})]});export{s as default};
